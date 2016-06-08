97fd62dd929e9e859c4609bf3466974843fbedb6
Marco Chávez <totto_cha@hotmail.com>
2016-06-08 01:40:06 -0600

    MCD y mcm utilizando recursividad

---
+++ MCD-AlgoritmoEuclides/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMCD = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.txtMCD = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnmcm = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnMCD
+        '
+        Me.btnMCD.Location = New System.Drawing.Point(29, 112)
+        Me.btnMCD.Name = "btnMCD"
+        Me.btnMCD.Size = New System.Drawing.Size(106, 23)
+        Me.btnMCD.TabIndex = 5
+        Me.btnMCD.Text = "Calcular MCD"
+        Me.btnMCD.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese el primer número"
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(172, 10)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(100, 20)
+        Me.txtA.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(135, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese el segundo número"
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(172, 57)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(100, 20)
+        Me.txtB.TabIndex = 4
+        '
+        'txtMCD
+        '
+        Me.txtMCD.Location = New System.Drawing.Point(172, 148)
+        Me.txtMCD.Name = "txtMCD"
+        Me.txtMCD.ReadOnly = True
+        Me.txtMCD.Size = New System.Drawing.Size(100, 20)
+        Me.txtMCD.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(172, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "El MCD es:"
+        '
+        'btnmcm
+        '
+        Me.btnmcm.Location = New System.Drawing.Point(29, 144)
+        Me.btnmcm.Name = "btnmcm"
+        Me.btnmcm.Size = New System.Drawing.Size(106, 23)
+        Me.btnmcm.TabIndex = 6
+        Me.btnmcm.Text = "Calcular mcm"
+        Me.btnmcm.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnmcm)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtMCD)
+        Me.Controls.Add(Me.txtB)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtA)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnMCD)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnMCD As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtA As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtB As System.Windows.Forms.TextBox
+    Friend WithEvents txtMCD As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnmcm As System.Windows.Forms.Button
+
+End Class
---
+++ MCD-AlgoritmoEuclides/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Private Sub btnMCD_Click(sender As Object, e As EventArgs) Handles btnMCD.Click
+
+        Dim a As Integer = txtA.Text
+        Dim b As Integer = txtB.Text
+
+        Dim MCD As Integer = getMCD(a, b)
+        txtMCD.Text = "MCD(" & a & "," & b & "): " & MCD
+
+        txtA.Text = ""
+        txtB.Text = ""
+
+    End Sub
+
+    Private Function getMCD(a As Integer, b As Integer) As Integer
+        Dim r As Integer = a Mod b
+
+        If r = 0 Then
+            Return b
+        Else
+            Return getMCD(b, r)
+        End If
+
+    End Function
+
+    Private Sub btnmcm_Click(sender As Object, e As EventArgs) Handles btnmcm.Click
+        Dim a As Integer = txtA.Text
+        Dim b As Integer = txtB.Text
+        Dim MCD As Integer = getMCD(a, b)
+
+        Dim mcm As Integer = (a * b) / MCD
+
+        txtMCD.Text = "mcm(" & a & "," & b & "): " & mcm
+
+        txtA.Text = ""
+        txtB.Text = ""
+
+    End Sub
+End Class
